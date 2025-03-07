50606f287420a89608db7d6d0298a2a6b8639866
Shruthi <ashruthi09@gmail.com>
2025-03-07 16:30:46 +0530

    First 5 multiples of 3

---
+++ src/main/scala/First5MultiplesOf3.scala
@@ -0,0 +1,10 @@
+object First5MultiplesOf3 {
+def main(args: Array[String]):Unit={
+  // 6) Generate and print first 5 multiples of 3 using a "for" loop
+  for(i<-1 to 5 by 1){
+    print(3*i + " ")
+  }
+
+}
+
+}
