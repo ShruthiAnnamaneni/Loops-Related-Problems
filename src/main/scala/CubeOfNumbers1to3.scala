d87cba5830d7cfaa9e45e50b790b2c97b9f3cbd2
Shruthi <ashruthi09@gmail.com>
2025-03-07 16:27:48 +0530

    Cube of numbers 1 to 3

---
+++ src/main/scala/CubeOfNumbers1to3.scala
@@ -0,0 +1,13 @@
+object CubeOfNumbers1to3 {
+  def main(args: Array[String]):Unit={
+    //14) Print the cube of numbers 1 to 3 using "while" loop
+    var i=1
+    while(i<=3){
+      print(i*i*i +" ")
+      i+=1
+    }
+
+
+  }
+
+}
