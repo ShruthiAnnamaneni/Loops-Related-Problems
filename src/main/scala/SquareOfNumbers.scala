36cdaf145c1d86f43aea3762ccc766af1a8996d8
Shruthi <ashruthi09@gmail.com>
2025-03-07 16:36:20 +0530

    Square of numbers

---
+++ src/main/scala/SquareOfNumbers.scala
@@ -0,0 +1,10 @@
+object SquareOfNumbers {
+  def main(args: Array[String]): Unit={
+    //4)Print the square of numbers from 1 to 5 using a "for" loop.
+    var i=1
+    while(i<=5){
+      print(i*i +" ")
+      i+=1
+    }
+  }
+}
